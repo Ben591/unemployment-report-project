ee4a4f95e8893e36d479c98dcfc49fd77c29dce8
Bd591 <bd591@georgetown.edu>
2022-11-02 18:19:47 -0400

    Update to stocks report code

--- app/stocks.py
+++ app/stocks.py
@@ -1,12 +1,13 @@
-
-# this is the "app/stocks.py" file...
-
 print("STOCKS REPORT...")
 
 from pandas import read_csv
+
 from app.alpha import API_KEY
 
-symbol = input("Please input a stock symbol (default: 'NFLX'): ") or "NFLX"
+def format_usd(my_price):
+    return f"${my_price:,.2f}"
+
+symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
 print("SYMBOL:", symbol)
 
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&datatype=csv"
@@ -23,12 +24,12 @@ latest = df.iloc[0]
 
 #print(latest["timestamp"])
 #print(latest["close"])
-print("LATEST:", '${:,.2f}'.format(latest["adjusted_close"]), "as of", latest["timestamp"])
+print("LATEST:", format_usd(latest["adjusted_close"]), "as of", latest["timestamp"])
 
 # Challenge B
 #
 # What is the highest high price (formatted as USD)?
 # What is the lowest low price (formatted as USD)?
 
-print("HIGH:", '${:,.2f}'.format(df["high"].max()))
-print("LOW:", '${:,.2f}'.format(df["low"].min()))
+print("HIGH:", format_usd(df["high"].max()))
+print("LOW:", format_usd(df["low"].min()))
